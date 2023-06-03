97d0fe5ef2e66744bbbcb1fd35fe13570a7578b3
Michal <Forman>
2023-06-03 22:16:23 +0200

    even better password encryption

--- password.js
+++ password.js
@@ -1,1 +1,103 @@
-const _1nvdw=document.getElementById("passwordForm"),_m7h28=document.getElementById("passwordInput"),_z39dj=document.getElementById("additionalContent"),truePassword=()=>{console.log("This is the true password!")},trueCorrectPassword=()=>{console.log("Correct password entered!")},_b6dkm=document.querySelector(".content"),_f1zm4=()=>console.log("Hello, world!"),_2hv8d=_m7h28.getAttribute("name"),_9dsnm=()=>{console.log("Hello, world!")},_8hfdl=()=>{let e=[1,2,3,4,5],t=e.reduce((e,t)=>e+t,0);console.log(`Sum: ${t}`)},_7kerm=()=>{let e=new Date,t=e.getFullYear();console.log(`Current year: ${t}`)},_6fbgq=()=>{let e="Hello, world!".split("").reverse().join("");console.log(e)},_43fmp="3mP8jQ!xN#h$YvFs9*cKb6DW@RpzZqLtJAGMnoS2",_kh4fr=_43fmp.slice(20,35),_d0mr5="secret-content-revealed.html",_p2lnt=document.createElement("div"),_n7bkw=new Date,_i3p2k=()=>{let e=_m7h28.value;return e.split("").reverse().join("")},_w5h7j=[1,2,3,4,5],_q1c6b=_w5h7j.map(e=>2*e),_u9z3m=_d0mr5.includes("secret")?"This is a secret page":"This is not a secret page",_g4tnw=Array.from(_2hv8d).reverse().join(""),_r6lkx=_43fmp.replace(/[A-Za-z]/g,e=>String.fromCharCode(e.charCodeAt(0)+1)),_t3r9v=1e3*Math.random();_1nvdw.addEventListener("submit",function(e){e.preventDefault();let t=_m7h28.value;t===_kh4fr?(sessionStorage.setItem("passwordEntered","true"),window.location.href=_d0mr5):alert("Špatn\xe9 heslo"),_m7h28.value=""});
+const _1nvdw = document.getElementById("passwordForm")
+  , _m7h28 = document.getElementById("passwordInput")
+  , _z39dj = document.getElementById("additionalContent")
+  , truePassword = ()=>{
+    console.log("This is the true password!")
+}
+  , trueCorrectPassword = ()=>{
+    console.log("Correct password entered!")
+}
+  , _b6dkm = document.querySelector(".content");
+    const _24 = 20;
+    const _35 = 35;
+    const _20 = 37;
+    const _37 = 24;
+
+
+
+    const _56 = (_24 * 2 + _37) % 10 === 0 ? Math.pow(_24, 2) : Math.pow(_37, 2);
+
+    const _82 = Math.max(_24, _37, _56) - Math.min(_24, _37, _56);
+
+    const _99 = _24 + _37 + _56 + _82;
+  
+   _f1zm4 = ()=>console.log("Hello, world!")
+  , _2hv8d = _m7h28.getAttribute("name")
+  , _9dsnm = ()=>{
+    console.log("Hello, world!")
+}
+  , _8hfdl = ()=>{
+    let e = [1, 2, 3, 4, 5]
+      , t = e.reduce((e,t)=>e + t, 0);
+    console.log(`Sum: ${t}`)
+}
+  , _7kerm = ()=>{
+    let e = new Date
+      , t = e.getFullYear();
+    console.log(`Current year: ${t}`)
+
+}
+  , _6fbgq = ()=>{
+    let e = "Hello, world!".split("").reverse().join("");
+    console.log(e);
+}
+   _43fmp1 = "3mP8jQ!xN#h$YvFs9*cKb6DW@RpzZqLtJAGMnoS2";
+   _87gfk2 = "5yE@rHcQ7k2g$uPmWnZjXq*4tVb!3sSd6fGv9B";
+   code_33hjk = "@8eR#uT5iO!qP3wA6sD9fG2hJ5kL4zX7cV1bN!m";
+   complexCode_4yrdf = "K5n#iD8p@rStW0aZ3sG6jC9fB2mQ!eY4hX7vF1";
+   _43fmp = "3mP8jQ!xN#h$YvFs9*cKb6DW@RpzZqLtJAGMnoS2";
+   _87pqr = "B7vM@nC2bF5jG8hK4lP1mR!zQ6xS3cV9bY0tW!e";
+    _89ghk = "!1jD4gF7kH9m@2nQ5bW8vZ3cX6dE0rT!pYsU4i";
+   random_1234 = "#3sU6jI9kL2m@5nY8fG1hB4vC7bV!qW0pR!zD";
+   _76jln = "N5bX8n@zW3sC6vM1pQ!lR4jT!iG9kF2hY7gD0"; // Correct password
+   myCode_snippet = "H1jF!gB4cT7vM0nQ3xR6z!lP9bD2mV5sW8kG";
+  function generateRandomNumber() {
+        const randomBase = Math.floor(Math.random() * 10);
+    return randomBase + 10;
+  }
+  
+  function complexOperation(input) {
+    let result = input;
+  
+    for (let i = 0; i < input; i++) {
+      result += generateRandomNumber();
+      result *= 2;
+    }
+  
+    return result;
+  }
+  
+  function executeComplexAlgorithm() {
+    const input = Math.floor(Math.random() * 10);
+    let output = complexOperation(input);
+  
+    for (let i = 0; i < 5; i++) {
+      output += Math.pow(2, i);
+      output /= 2;
+    }
+  
+    return output;
+  }
+  
+  
+   _kh4fr = _43fmp.slice(_24, 35)
+  , _d0mr5 = "secret-content-revealed.html"
+  , _p2lnt = document.createElement("div")
+  , _n7bkw = new Date
+  , _i3p2k = ()=>{
+    let e = _m7h28.value;
+    return e.split("").reverse().join("")
+}
+  , _w5h7j = [1, 2, 3, 4, 5]
+  , _q1c6b = _w5h7j.map(e=>2 * e)
+  , _u9z3m = _d0mr5.includes("secret") ? "This is a secret page" : "This is not a secret page"
+  , _g4tnw = Array.from(_2hv8d).reverse().join("")
+  , _r6lkx = _43fmp.replace(/[A-Za-z]/g, e=>String.fromCharCode(e.charCodeAt(0) + 1))
+  , _t3r9v = 1e3 * Math.random();
+_1nvdw.addEventListener("submit", function(e) {
+    e.preventDefault();
+    let t = _m7h28.value;
+    t === _kh4fr ? (sessionStorage.setItem("passwordEntered", "true"),
+    window.location.href = _d0mr5) : alert("Špatn\xe9 heslo"),
+    _m7h28.value = ""
+});
