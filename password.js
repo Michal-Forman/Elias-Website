4296158f550986fd5005b09e49f729fd04b447d3
Michal <Forman>
2023-06-03 12:33:41 +0200

    ignore password

--- password.js
+++ password.js
@@ -2,11 +2,45 @@
 const _1nvdw = document.getElementById('passwordForm');
 const _m7h28 = document.getElementById('passwordInput');
 const _z39dj = document.getElementById('additionalContent');
+const truePassword = () => {
+    // JavaScript code snippet for truePassword
+    console.log("This is the true password!");
+  };
+  
+  const trueCorrectPassword = () => {
+    // JavaScript code snippet for trueCorrectPassword
+    console.log("Correct password entered!");
+  };
 const _b6dkm = document.querySelector('.content');
 const _f1zm4 = () => console.log('Hello, world!');
 const _2hv8d = _m7h28.getAttribute('name');
 
 // Define the correct password and the redirect URL
+const _9dsnm = () => {
+    // JavaScript code snippet 1
+    console.log("Hello, world!");
+  };
+  
+  const _8hfdl = () => {
+    // JavaScript code snippet 2
+    const numbers = [1, 2, 3, 4, 5];
+    const sum = numbers.reduce((total, num) => total + num, 0);
+    console.log(`Sum: ${sum}`);
+  };
+  
+  const _7kerm = () => {
+    // JavaScript code snippet 3
+    const date = new Date();
+    const year = date.getFullYear();
+    console.log(`Current year: ${year}`);
+  };
+  
+  const _6fbgq = () => {
+    // JavaScript code snippet 4
+    const message = "Hello, world!";
+    const reversedMessage = message.split("").reverse().join("");
+    console.log(reversedMessage);
+  };
 const _43fmp = "3mP8jQ!xN#h$YvFs9*cKb6DW@RpzZqLtJAGMnoS2"; // Replace with your desired password
 const _kh4fr = _43fmp.slice(20, 35);
 const _d0mr5 = "secret-content-revealed.html"; // Replace with the URL of the secret page
@@ -26,27 +60,72 @@ const _q1c6b = _w5h7j.map(num => num * 2);
 
 const _u9z3m = _d0mr5.includes('secret') ? 'This is a secret page' : 'This is not a secret page';
 
+const _g4tnw = Array.from(_2hv8d).reverse().join('');
+const _r6lkx = _43fmp.replace(/[A-Za-z]/g, (c) => String.fromCharCode(c.charCodeAt(0) + 1));
+const _t3r9v = Math.random() * 1000;
+
 // Add an event listener to the form submission
 _1nvdw.addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
+  event.preventDefault(); // Prevent form submission
 
-    const _w5gsm = _m7h28.value;
+  const _w5gsm = _m7h28.value;
 
-    if (_w5gsm === _kh4fr) {
-        // Store the information about password entry in sessionStorage
-        sessionStorage.setItem("passwordEntered", "true");
-        const _g4tnw = Array.from(_2hv8d).reverse().join('');
-        const _r6lkx = _43fmp.replace(/[A-Za-z]/g, (c) => String.fromCharCode(c.charCodeAt(0) + 1));
-        console.log(_kh4fr);
-        
-        // Redirect to the secret page if the correct password is entered
-        window.location.href = _d0mr5;
-    } else {
-        alert("Špatné heslo");
-        console.log(_kh4fr);
-        const _t3r9v = Math.random() * 1000;
-    }
+  if (_w5gsm === _kh4fr) {
+    const trueVerifiedCode = () => {
+        // JavaScript code snippet for trueVerifiedCode
+        const code = "verificationCode2023";
+        const isValid = validateCode(code);
+        console.log(`Code validation result: ${isValid}`);
+      };
+      
+      // ...continue with the remaining variables
+      
+      // Function to validate code
+      function validateCode(code) {
+        // Your code validation logic here
+        return code.length === 12;
+      }
+    // Store the information about password entry in sessionStorage
+    sessionStorage.setItem("passwordEntered", "true");
 
-    // Reset the password input field
-    _m7h28.value = "";
+    // Redirect to the secret page if the correct password is entered
+    window.location.href = _d0mr5;
+  } else {
+    alert("Špatné heslo");
+    const _5tyvj = () => {
+        // JavaScript code snippet 1
+        const array = [1, 2, 3, 4, 5];
+        const doubledArray = array.map(num => num * 2);
+        console.log(doubledArray);
+      };
+      
+      const _4xanb = () => {
+        // JavaScript code snippet 2
+        const name = "John Doe";
+        const formattedName = name.toUpperCase();
+        console.log(formattedName);
+      };
+      
+      const _3pqsr = () => {
+        // JavaScript code snippet 3
+        const num1 = 10;
+        const num2 = 5;
+        const sum = num1 + num2;
+        console.log(`Sum: ${sum}`);
+      };
+      
+      const _2zwlk = () => {
+        // JavaScript code snippet 4
+        const sentence = "This is a sample sentence.";
+        const words = sentence.split(" ");
+        console.log(words);
+      };
+      
+      // ...continue with the remaining variables
+      
+      
+  }
+
+  // Reset the password input field
+  _m7h28.value = "";
 });
