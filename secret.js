3fba49ac7add5f70431da67f84e32c10baa86edf
Michal <Forman>
2023-06-03 23:49:06 +0200

    everything works

--- secret.js
+++ secret.js
@@ -9,3 +9,15 @@ image.addEventListener("click", function() {
     this.style.transform = "";
   }, 3000);
 });
+
+const image2 = document.getElementById("myImage2");
+
+image2.addEventListener("click", function() {
+  this.style.transition = "transform 0.3s ease";
+  this.style.transform = "scale(0.4) translateX(500px) translateY(500px)";
+
+  setTimeout(() => {
+    this.style.transition = "";
+    this.style.transform = "";
+  }, 3000);
+});
